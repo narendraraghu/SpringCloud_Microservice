4253e0d44ff00c424e9c66b858d6443300c26149
narendrakareli <narendraraghu.89@gmail.com>
2018-01-02 23:32:25 +0530

    Initial commit

---
+++ src/main/java/com/cloud/toll/SpringCloudWithMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.toll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudWithMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudWithMicroserviceApplication.class, args);
+	}
+}
